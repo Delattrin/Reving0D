4837634925a46f2d59e82496035472e3ba157e93
Delattrin <154396718+Delattrin@users.noreply.github.com>
2023-12-20 01:06:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function abreLink(){
+    var entrada = document.getElementById('entrada');
+    var senha = document.getElementById('senha');
+
+    if ((entrada.value == 'Nath' || entrada.value == "Nath") && senha.value == '03122023') {
+        localStorage.setItem("acesso", true);
+        window.location.href = "../Pasta2/pag2.html"
+    }else {
+        alert("Usuário ou senha não aceitas bb")
+    }
+}
